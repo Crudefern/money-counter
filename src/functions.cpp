cffc7a909b14d8d09baeaf74800a2682dc694932
TheDuckBoi <115040941+TheDuckBoi@users.noreply.github.com>
2022-12-31 21:15:32 -0500

    modified:   src/functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -18,15 +18,15 @@ void readButtons() {
   //read the buttons
   float to_add = 0;
   if (add == true) {
-    if (digitalRead(C1)) {to_add = to_add + 0.01;}
-    if (digitalRead(C10)) {to_add = to_add + 0.10;}
-    if (digitalRead(C25)) {to_add + to_add + 0.25;}
-    if (digitalRead(S1)) {to_add = to_add + 1;}
+    if (digitalRead(C1)) {to_add += 0.01;}
+    if (digitalRead(C10)) {to_add += 0.10;}
+    if (digitalRead(C25)) {to_add += 0.25;}
+    if (digitalRead(S1)) {to_add += 1;}
   } else {
-    if (digitalRead(C1)) {to_add = to_add - 0.01;}
-    if (digitalRead(C10)) {to_add = to_add - 0.10;}
-    if (digitalRead(C25)) {to_add = to_add - 0.25;}
-    if (digitalRead(S1)) {to_add = to_add - 1;}
+    if (digitalRead(C1)) {to_add -= 0.01;}
+    if (digitalRead(C10)) {to_add -= 0.10;}
+    if (digitalRead(C25)) {to_add -= 0.25;}
+    if (digitalRead(S1)) {to_add -= 1;}
   }
 
   money = money + to_add;
