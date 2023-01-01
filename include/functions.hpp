1c2b3d648766a21db7a7941c9af0b518f6f871aa
TheDuckBoi <115040941+TheDuckBoi@users.noreply.github.com>
2022-12-31 21:31:59 -0500

    modified:   include/functions.hpp 	modified:   src/functions.cpp

--- include/functions.hpp
+++ include/functions.hpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 
 //button pins
-extern const int C1=10; //adds 1 cent
-extern const int C10=9; //adds 10 cents
-extern const int C25=8; //adds 25 cents
-extern const int S1=7;  //adds 1 dollar
+extern const int C1;
+extern const int C10;
+extern const int C25;
+extern const int S1;
 
-extern const int addr = 0;
+extern const int addr;
 extern float money; 
 extern float oldMoney;
-extern unsigned long time = millis();
-extern unsigned long oldTime = time;
-extern bool add = true;
+extern unsigned long time;
+extern unsigned long oldTime;
+extern bool add;
 
 void eepromClear();
 void readButtons();
--- src/functions.cpp
+++ src/functions.cpp
@@ -2,6 +2,16 @@
 #include <EEPROM.h>
 #include "functions.hpp"
 
+const int C1=10; //adds 1 cent
+const int C10=9; //adds 10 cents
+const int C25=8; //adds 25 cents
+const int S1=7;  //adds 1 dollar
+const int addr = 0;
+unsigned long time = millis();
+unsigned long oldTime = time;
+bool add = true;
+
+
 void ledBlink(int blinkTime) {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(blinkTime);
