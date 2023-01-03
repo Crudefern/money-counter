a9b8840a2a3fb9388eab874dc1f6b34aab96d948
TheDuckBoi <115040941+TheDuckBoi@users.noreply.github.com>
2023-01-03 14:58:36 -0500

    remove debug code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <EEPROM.h>
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 #include "functions.hpp"
 #include "EEvar.h"
 //setup values (change pin numbers in src/functions.cpp)
@@ -9,12 +9,12 @@ const float wantMoney = 242.98; // set this to the target amout of money you wan
 // A tinytesla with spare IGBTs and the discount code ELECTROBOOM10 costs $242.98
 // An oculus quest 2 256GB with the standard strap is $429.99
 
-SoftwareSerial mySerial(-1, 4); // RX, TX (TX is XTAL2/TP2)
+// SoftwareSerial mySerial(-1, 4); // RX, TX (TX is XTAL2/TP2)
 
 const EEstore<float> eeMoney(0);
 
 void setup() {
-  mySerial.begin(9600); //for debugging
+  // mySerial.begin(9600); //for debugging
   setupPins(); //set the pin states
   delay(50);
   if (!digitalRead(C1) && !digitalRead(C10) && !digitalRead(C25) && !digitalRead(S1)) {eepromClear();}
@@ -28,7 +28,7 @@ void loop() {
   readButtons();
   // write to the EEPROM 
   if (money != oldMoney && time - oldTime >= timeout) {
-    mySerial.println("WRITING");
+    // mySerial.println("WRITING");
     oldMoney = money;
     oldTime = time;
     eeMoney << money; // write money count to eeprom
@@ -37,8 +37,8 @@ void loop() {
   // blink when you have enough money
   if (money >= wantMoney) {while (true) {multiBlink(30,32767);}}
   if (money < 0) {money = 0;}
-  mySerial.println(money); //debugging
-  mySerial.println(oldMoney); //debugging
+  // mySerial.println(money); //debugging
+  // mySerial.println(oldMoney); //debugging
 }
 
 
