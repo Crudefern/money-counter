fd86533664d221a3b0019710b68e32da4ab01cf1
TheDuckBoi <115040941+TheDuckBoi@users.noreply.github.com>
2023-01-08 22:28:45 -0500

    prettier-ify code

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,36 @@
 #include "functions.hpp"
 // #define EE_TEST_VAL 0x315A // uncomment when changing the hardcoded wantmoney
 #include "EEvar.h"
-//setup values (change pin numbers in src/functions.cpp)
+// setup values (change pin numbers in src/functions.cpp)
 const int timeout = 1000; // time in between the last button press and eeprom write
-#define initialWantMoney 242.98 // set this to the target amout of money you want
+#define initialWantMoney 499.99f // set this to the target amout of money you want
 // A tinytesla with spare IGBTs and the discount code ELECTROBOOM10 costs $242.98
-// An oculus quest 2 256GB with the standard strap is $429.99
- 
+// An oculus quest 2 256GB with the standard strap is $499.99
+
 const EEstore<float> eeMoney(0);
 const EEstore<float> eeWantMoney(initialWantMoney);
 
 void eepromClear() { // this has to be put here because of the eevar library
   money = 0;
   eeMoney << money;
-  multiBlink(500,3);
+  multiBlink(500, 3);
 }
 
-void setup() {
+void setup()  {
   setupPins(); // set the pin states
   delay(50);
-  if (!digitalRead(C1) && !digitalRead(C10) && !digitalRead(C25) && !digitalRead(S1)) {eepromClear();} // clear the eeprom
-  if (!digitalRead(C25) && !digitalRead(S1)) {while (true) {wantMoney = 0;wantMoney += readButtons(); // this line and the line below set and save wantMoney
-    if (!digitalRead(C1) && !digitalRead(C10) && !digitalRead(C25) && !digitalRead(S1)) {eeWantMoney << wantMoney;while (true) {delay(1);
-    }}}
+  if (!digitalRead(C1) && !digitalRead(C10) && !digitalRead(C25) && !digitalRead(S1))  {eepromClear();} // clear the eeprom
+  if (!digitalRead(C25) && !digitalRead(S1)) {
+    while (true) {
+      wantMoney = 0;
+      wantMoney += readButtons(); // this line and the line below set and save wantMoney
+      if (!digitalRead(C1) && !digitalRead(C10) && !digitalRead(C25) && !digitalRead(S1)) {
+        eeWantMoney << wantMoney;
+        while (true) {
+          delay(1);
+        }
+      }
+    }
   }
   eeMoney >> money; // get money count from eeprom
   eeWantMoney >> wantMoney;
@@ -36,15 +44,17 @@ void loop() {
   time = millis();
   money += readButtons(); // get button states
 
-  // write to the EEPROM 
-    if (money != oldMoney && time - oldTime >= timeout) {
+  // write to the EEPROM
+  if (money - oldMoney > 0.001 && time - oldTime >= timeout)  {
     oldMoney = money;
     oldTime = time;
     eeMoney << money; // write money count to eeprom
-    multiBlink(100,3);
+    multiBlink(100, 3);
   }
 
   // blink when you have enough money
-  if (money >= wantMoney) {while (true) {multiBlink(30,32767);}}
+  if (money >= wantMoney)  {
+    while (true) {multiBlink(30, 32767);}
+  }
   if (money < 0) {money = 0;} // a failsafe
 }
